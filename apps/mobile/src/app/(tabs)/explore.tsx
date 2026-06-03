494850087f37fd7cb18d115399df4b2dd476e966
saiteja-madha <git@saitejamadha.dev>
2026-06-02 22:52:25 -0700

    refactor(mobile): restructure app to src directory and update paths

--- apps/mobile/src/app/(tabs)/explore.tsx
+++ apps/mobile/src/app/(tabs)/explore.tsx
@@ -66,7 +66,7 @@ export default function TabTwoScreen() {
           provide files for different screen densities
         </ThemedText>
         <Image
-          source={require("@/assets/images/react-logo.png")}
+          source={require("@/../assets/images/react-logo.png")}
           style={{ width: 100, height: 100, alignSelf: "center" }}
         />
         <ExternalLink href="https://reactnative.dev/docs/images">
